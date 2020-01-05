c5c428b947dca7be016f1ae1c768abbbf09a758e
pallav <ayush.pallav@innovaccer.com>
2020-01-05 23:26:14 +0530

    Dockerized microservice initial setup

---
+++ myJournal/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for myJournal project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+import dotenv
+
+dotenv.load_dotenv(os.path.join( os.path.dirname( (os.path.dirname(__file__)) ), '.env' ))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myJournal.settings')
+
+application = get_wsgi_application()
