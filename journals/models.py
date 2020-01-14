0e0fe7e560843de1217bbb9f66ff260d52024817
pallav <ayush.pallav@innovaccer.com>
2020-01-15 02:44:09 +0530

    model for journals with microservice bugs fixed

--- journals/models.py
+++ journals/models.py
@@ -1,3 +1,48 @@
+"""
+Each profile has a unique journal with jourrnal_id assigned to it
+Each journal can have multiple entries, hence a one-to-many field from journal to entries
+"""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.contrib.postgres.fields import JSONField
+
+from django.conf import settings
+
+
+class Profile(models.Model):
+    """
+    Extends user model of django.auth
+    stores further information needed for a user
+    """
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    email_id = models.EmailField(max_length=254)
+
+
+class Journal(models.Model):
+    """
+    Model to keep journal for each profile
+    """
+    profile = models.OneToOneField(
+        Profile,
+        on_delete=models.DO_NOTHING
+    )
+
+
+class Entry(models.Model):
+    """
+    Model for daily cronological entries
+    A new entry is to be made every day
+    Each 'daily entry' will contain key value pair of time and entry
+    """
+    date = models.DateTimeField()
+    entries = JSONField(
+        help_text="key value pair (time, entry) for each day"
+    )
+    journal = models.ForeignKey(
+        Journal,
+        on_delete=models.DO_NOTHING
+    )
